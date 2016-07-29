e2be69d4f92a1e1f7804b466619247a5e5270a9d
Fernando Masini <fsmasini@gmail.com>
2016-07-29 19:09:46 -0300

    Criação UsuárioDAO, DAO(Template), revisão usings do User;

---
+++ Financial/DAO/DAO.cs
@@ -0,0 +1,21 @@
+﻿using Financial.Models.Context;
+using System.Collections.Generic;
+
+namespace Financial.DAO
+{
+    public abstract class DAO<T>
+    {
+        protected FinancialContext Context { get; private set; }
+
+        public DAO(FinancialContext context)
+        {
+            this.Context = context;
+        }
+
+        public abstract List<T> List();
+        public abstract void Add(T entity);
+        public abstract void Update(T entity);
+        public abstract void Delete(T entity);
+        public abstract T GetById(int entityId);
+    }
+}
---
+++ Financial/DAO/UserDAO.cs
@@ -0,0 +1,39 @@
+﻿using Financial.Models.Context;
+using Financial.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financial.DAO
+{
+    public class UserDAO : DAO<User>
+    {
+        public UserDAO(FinancialContext context) : base(context) { }
+
+        public override List<User> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Add(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override User GetById(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
