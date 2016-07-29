7d652be4891eb2fc0a9eac3897e6415dad7f6236
Fernando Masini <fsmasini@gmail.com>
2016-07-29 18:55:57 -0300

    Criada a entidade Usuário

---
+++ Financial/Models/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Financial.Models.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public String Name { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public String Login { get; set; }
+    }
+}
