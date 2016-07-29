205abac0931918db0d06ac43a18f7100806fe7f6
Fernando Masini <fsmasini@gmail.com>
2016-07-29 20:09:54 -0300

    Adicionado Bootswatch Cosmo; Adicionado Font-Awesome; Adicionado site.css; Modificada Home/Index; Separação do Menu em Controller/Views;

---
+++ Financial/Controllers/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Financial.Controllers
+{
+    [ChildActionOnly]
+    public class MenuController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult LoginControl()
+        {
+            return PartialView();
+        }
+
+        public ActionResult UserControl()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Navigation()
+        {
+            return PartialView();
+        }
+    }
+}
