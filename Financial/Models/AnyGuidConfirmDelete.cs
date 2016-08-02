66c0b3d711b3a085738768b86bd3a2576999e674
Fernando Masini <fsmasini@gmail.com>
2016-08-02 13:13:02 -0300

    Criado Model genérico para deleções usando Guid;

---
+++ Financial/Models/AnyGuidConfirmDelete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financial.Models
+{
+    public class AnyGuidConfirmDelete
+    {
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+    }
+}
