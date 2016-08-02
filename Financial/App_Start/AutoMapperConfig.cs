6fe8351b657a70d2a8be924759be6f88c42b2844
Fernando Masini <fsmasini@gmail.com>
2016-08-02 19:33:42 -0300

    Adicionado Mapeamentos de Purchase;

--- Financial/App_Start/AutoMapperConfig.cs
+++ Financial/App_Start/AutoMapperConfig.cs
@@ -13,6 +13,7 @@ namespace Financial.App_Start
     {
         public static void ConfigureAutoMapper()
         {
+            #region :: Credit Card Maps ::
             Mapper.CreateMap<CreditCard, CreditCardFormModel>();
             Mapper.CreateMap<CreditCardFormModel, CreditCard>().
                 ForMember(c => c.Network, o =>
@@ -25,13 +26,41 @@ namespace Financial.App_Start
                     o.MapFrom(m => new User() { Id = WebSecurity.CurrentUserId });
                 });
 
-            Mapper.CreateMap<CreditCard, AnyGuidConfirmDelete>().ForMember(c => c.Name, o => o.MapFrom(m => m.Nickname));
-            Mapper.CreateMap<AnyGuidConfirmDelete,CreditCard>().
-                ForMember(c=> c.Nickname, o=> o.MapFrom(m=>m.Name)).
+            Mapper.CreateMap<CreditCard, AnyGuidConfirmDelete>().
+                ForMember(c => c.Name, o => o.MapFrom(m => m.Nickname));
+            Mapper.CreateMap<AnyGuidConfirmDelete, CreditCard>().
+                ForMember(c => c.Nickname, o => o.MapFrom(m => m.Name)).
                 ForMember(c => c.Owner, o =>
                 {
                     o.MapFrom(m => new User() { Id = WebSecurity.CurrentUserId });
                 });
+
+            #endregion
+
+            #region :: Purchase Maps ::
+
+            Mapper.CreateMap<Purchase, PurchaseFormModel>();
+            Mapper.CreateMap<PurchaseFormModel, Purchase>().
+                ForMember(p => p.CreditCard, o =>
+                {
+                    o.MapFrom(m => new CreditCard() { Id = m.CreditCardId ?? Guid.Empty });
+                    o.Condition(m => m.CreditCardId != Guid.Empty);
+                }).
+                ForMember(c => c.Owner, o =>
+                {
+                    o.MapFrom(m => new User() { Id = WebSecurity.CurrentUserId });
+                });
+
+            Mapper.CreateMap<Purchase, AnyGuidConfirmDelete>().
+                ForMember(p => p.Name, o => o.MapFrom(m => m.Description));
+            Mapper.CreateMap<AnyGuidConfirmDelete, Purchase>().
+                ForMember(p => p.Description, o => o.MapFrom(m => m.Name)).
+                ForMember(p => p.Owner, o =>
+                {
+                    o.MapFrom(m => new User() { Id = WebSecurity.CurrentUserId });
+                });
+
+            #endregion
         }
     }
 }
