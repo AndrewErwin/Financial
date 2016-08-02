59ef2a3239bfec8f0888d8ac696c6856a302e84b
Fernando Masini <fsmasini@gmail.com>
2016-08-02 11:39:51 -0300

    Modificação no Mapeamento do CreditCard para atribuir o Owner com base no Usuário Logado;

--- Financial/App_Start/AutoMapperConfig.cs
+++ Financial/App_Start/AutoMapperConfig.cs
@@ -5,6 +5,7 @@ using System.Web;
 using AutoMapper;
 using Financial.Models;
 using Financial.Models.Entities;
+using WebMatrix.WebData;
 
 namespace Financial.App_Start
 {
@@ -18,7 +19,11 @@ namespace Financial.App_Start
                 {
                     o.MapFrom(m => new CreditCardNetwork { Id = m.NetworkId });
                     o.Condition(m => m.NetworkId != Guid.Empty);
-                });                
+                }).
+                ForMember(c => c.Owner, o =>
+                {
+                    o.MapFrom(m => new User() { Id = WebSecurity.CurrentUserId });
+                });
         }
     }
 }
