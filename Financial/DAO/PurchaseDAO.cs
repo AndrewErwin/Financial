0418a2a6e0b8d9c6c03a9537f19793a645acae24
Fernando Masini <fsmasini@gmail.com>
2016-08-03 16:15:35 -0300

    Adicionado PurchaseDAO;

---
+++ Financial/DAO/PurchaseDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Financial.Models.Context;
+using Financial.Models.Entities;
+using Microsoft.Data.Entity;
+using WebMatrix.WebData;
+
+namespace Financial.DAO
+{
+    public class PurchaseDAO : DAO<Purchase, Guid>
+    {
+        public PurchaseDAO(FinancialContext context) : base(context) { }
+
+        public override List<Purchase> List(params System.Linq.Expressions.Expression<Func<Purchase, object>>[] includes)
+        {
+            var purchases = this.Context.Purchases;
+            return includes.Aggregate(
+                        purchases.AsQueryable(),
+                        (query, include) => query.Include(include)
+                    ).Where(c => c.OwnerId == WebSecurity.CurrentUserId).OrderBy(c => c.Description).ToList();
+        }
+
+        public override void Add(Purchase entity)
+        {
+            entity.OwnerId = WebSecurity.CurrentUserId;
+            this.Context.Purchases.Add(entity);
+            this.Context.SaveChanges();
+        }
+
+        public override void Update(Purchase entity)
+        {
+            this.Context.Purchases.Update(entity);
+            this.Context.SaveChanges();
+        }
+
+        public override void Delete(Purchase entity)
+        {
+            this.Context.Purchases.Remove(entity);
+            this.Context.SaveChanges();
+        }
+
+        public override Purchase GetById(Guid entityId, params System.Linq.Expressions.Expression<Func<Purchase, object>>[] includes)
+        {
+            var purchase = this.Context.Purchases;
+            return includes.Aggregate(
+                        purchase.AsQueryable(),
+                        (query, include) => query.Include(include)
+                    ).Where(c => c.OwnerId == WebSecurity.CurrentUserId).FirstOrDefault();
+        }
+    }
+}
