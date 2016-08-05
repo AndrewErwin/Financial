9871e47deaa144e61cdeac04815f00f3bbd1b0fc
Fernando Masini <fsmasini@gmail.com>
2016-08-05 18:51:42 -0300

    Adicionado Controller Subscription; Adicionado View List Subscription;

---
+++ Financial/Controllers/SubscriptionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Financial.DAO;
+using Financial.Models.Entities;
+
+namespace Financial.Controllers
+{
+    [Authorize]
+    public class SubscriptionController : Controller
+    {
+        private SubscriptionDAO subscriptionDAO;
+        private CreditCardDAO creditCardDAO;
+
+        public SubscriptionController(SubscriptionDAO subscriptionDao, CreditCardDAO creditCardDao)
+        {
+            this.subscriptionDAO = subscriptionDao;
+            this.creditCardDAO = creditCardDao;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return RedirectToRoute("ListSubscriptions");
+        }
+
+        [HttpGet]
+        [Route("Subscriptions")]
+        [Route("List/Subscription")]
+        [Route("Subscription/List", Name = "ListSubscriptions")]
+        public ActionResult List()
+        {
+            return View(subscriptionDAO.List(s => s.CreditCard));
+        }
+    }
+}
